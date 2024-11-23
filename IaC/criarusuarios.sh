44132c6aa75aaf8c209d48366e3e07e6648c0ab8
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2024-11-23 19:17:32 -0300

    Update criarusuarios.sh

--- IaC/criarusuarios.sh
+++ IaC/criarusuarios.sh
@@ -9,21 +9,26 @@ mkdir /sec
 
 echo "Criando grupos de usuários..."
 
-groupadd GRP_ADM GRP_VEN GRP_SEC
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
 
-echo "criando usuários..."
+echo "Criando usuários e atribuindo grupos..."
 
+# Usuários do grupo ADM
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+# Usuários do grupo VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+# Usuários do grupo SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
 echo "Especificando permissões dos diretórios..."
 
